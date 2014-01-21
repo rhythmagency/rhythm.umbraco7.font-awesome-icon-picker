e9188972c71166b41b77921be52fb7b9e44fa92c
Doug Kulak <doug.kulak@rhythmagency.com>
2014-01-21 10:37:02 -0800

    Adding property editor sources and updating README.

---
+++ FontAwesomeIconPicker/fontawesomeiconpicker.controller.js
@@ -0,0 +1,26 @@
+﻿angular.module("umbraco")
+    .controller("Rhythm.FontAwesomeIconPickerController",
+        function($scope, assetsService) {
+            var PLUGIN_PATH = "/App_Plugins/FontAwesomeIconPicker";
+
+            assetsService.load([
+                    PLUGIN_PATH + '/vendor/select2/select2.min.js',
+                ])
+                .then(function() {
+                    var format = function(value) {
+                            return '<i class="fa ' + value.text + '"></i> ' + value.text;
+                        },
+                        options = {
+                            formatSelection: format,
+                            formatResult: format,
+                            escapeMarkup: function(m) { return m; },
+                            width: 'element'
+                        };
+
+                    $('.font-awesome-icon-picker').select2(options);
+                });
+
+            assetsService.loadCss(PLUGIN_PATH + '/vendor/select2/select2.css');
+            assetsService.loadCss(PLUGIN_PATH + '/vendor/fontawesome/fontawesome.min.css');
+            assetsService.loadCss(PLUGIN_PATH + '/fontawesomeiconpicker.css');
+        });
--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-rhythm.umbraco7.font-awesome-icon-picker
-========================================
+Font Awesome Icon Picker
+==
+_Property Editor for Umbraco 7 (Belle)_ by [Rhythm Interactive]
 
-Font Awesome icon picker for Umbraco 7
+Allows for the selection of a Font Awesome icon from a searchable dropdown. The class of the selected Font Awesome icon is saved as the property value. This property editor is particularly useful for allowing users to select icons for navigation items in menus.  
+
+![Example](http://oi41.tinypic.com/2ugmdfn.jpg)
+
+Tech
+---
+
+This property editor uses the following open source projects to work properly:
+
+* [Font Awesome] - awesome font-based icons 
+* [Select2] - an awesome jQuery based replacement for select boxes
+
+Installation
+---
+Simply copy the folder into the **App_Plugins** folder of your Umbraco instance and restart the application. Then, you should be able to create a new data type and select "Rhythm: Font Awesome Icon Picker". 
+
+License
+----
+MIT
+
+**Free Software, Heck Yeah!**
+
+[Font Awesome]:http://fontawesome.io/
+[Select2]:http://ivaynberg.github.io/select2/
+[Rhythm Interactive]:http://rhythmagency.com
