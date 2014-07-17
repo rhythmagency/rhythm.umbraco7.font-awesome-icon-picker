bb822b6fdbba4ba9bb29a3bd99755bde42d3c00f
Collyn Philleo <collyn.philleo@rhythmagency.com>
2014-07-16 17:11:20 -0700

    Fixing initial model value not displaying when used within an Archetype fieldset

--- FontAwesomeIconPicker/fontawesomeiconpicker.controller.js
+++ FontAwesomeIconPicker/fontawesomeiconpicker.controller.js
@@ -1,29 +1,41 @@
 ﻿angular.module("umbraco")
     .controller("Rhythm.FontAwesomeIconPickerController",
-        function($scope, assetsService) {
-            var PLUGIN_PATH = "/App_Plugins/FontAwesomeIconPicker";
+        function($scope) {
+            //empty for now
+        });
 
-            assetsService.load([
-                    PLUGIN_PATH + '/vendor/select2/select2.min.js',
-                ])
-                .then(function() {
-                    var format = function(value) {
-                            return '<i class="fa ' + value.text + '"></i> ' + value.text;
-                        },
-                        options = {
-                            formatSelection: format,
-                            formatResult: format,
-                            escapeMarkup: function(m) { return m; },
-                            width: 'element',
-                            allowClear: true,
-                            placeholder: "None"
-                        };
+angular.module('umbraco')
+    .directive('fontAwesomeIconPicker', function(assetsService) {
+    return {
+        restrict: 'C', //matches class 'font-awesome-icon-picker'
+        link: function(scope, el, attrs) {
 
-                    $('select.font-awesome-icon-picker').select2(options);
-                    $('select.font-awesome-icon-picker').select2("val", $scope.model.value);
+                var PLUGIN_PATH = "/App_Plugins/FontAwesomeIconPicker";
+
+                assetsService.load([
+                        PLUGIN_PATH + '/vendor/select2/select2.min.js',
+                    ])
+                    .then(function() {
+                        var format = function(value) {
+                                return '<i class="fa ' + value.text + '"></i> ' + value.text;
+                            },
+                            options = {
+                                formatSelection: format,
+                                formatResult: format,
+                                escapeMarkup: function(m) { return m; },
+                                width: 'element'
+                            };
+
+                        //Ensure the custom select box loads after the first Angular digest
+                        //so that the initial value matches model.value
+                        setTimeout(function() {
+                            $(el).select2(options);
+                        }, 0);
                 });
 
-            assetsService.loadCss(PLUGIN_PATH + '/vendor/select2/select2.css');
-            assetsService.loadCss(PLUGIN_PATH + '/vendor/fontawesome/fontawesome.min.css');
-            assetsService.loadCss(PLUGIN_PATH + '/fontawesomeiconpicker.css');
-        });
+                assetsService.loadCss(PLUGIN_PATH + '/vendor/select2/select2.css');
+                assetsService.loadCss(PLUGIN_PATH + '/vendor/fontawesome/fontawesome.min.css');
+                assetsService.loadCss(PLUGIN_PATH + '/fontawesomeiconpicker.css');
+        }
+    };
+});
